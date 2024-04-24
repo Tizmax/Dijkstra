1a346d123fe9cf420368c5262799d09f14ae7b74
mbi5737 <matthis-bernardini@etu.inp-n7.fr>
2024-04-24 20:19:30 +0200

    TEST

---
+++ dijkstra.c
@@ -0,0 +1,61 @@
+#include "dijkstra.h"
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * construire_chemin_vers - Construit le chemin depuis le noeud de départ donné vers le
+ * noeud donné. On passe un chemin en entrée-sortie de la fonction, qui est mis à jour
+ * par celle-ci.
+ *
+ * Le noeud de départ est caractérisé par un prédécesseur qui vaut `NO_ID`.
+ *
+ * Ce sous-programme fonctionne récursivement :
+ *  1. Si le noeud a pour précédent `NO_ID`, on a fini (c'est le noeud de départ, le chemin de
+ *     départ à départ se compose du simple noeud départ)
+ *  2. Sinon, on construit le chemin du départ au noeud précédent (appel récursif)
+ *  3. Dans tous les cas, on ajoute le noeud au chemin, avec les caractéristiques associées dans visites
+ *
+ * @param chemin [in/out] chemin dans lequel enregistrer les étapes depuis le départ vers noeud
+ * @param visites [in] liste des noeuds visités créée par l'algorithme de Dijkstra
+ * @param noeud noeud vers lequel on veut construire le chemin depuis le départ
+ */
+// TODO: construire_chemin_vers
+
+
+float dijkstra(
+    const struct graphe_t* graphe, 
+    noeud_id_t source, noeud_id_t destination, 
+    liste_noeud_t** chemin) {
+    
+    liste_noeud_t* AVisiter = creer_liste();
+    liste_noeud_t* Visites = creer_liste();
+
+    inserer_noeud_liste(AVisiter, source, NO_ID, INFINITY);
+
+    while (est_vide_liste(AVisiter))
+    {
+        noeud_id_t nc = min_noeud_liste(AVisiter);
+        noeud_id_t precedent_nc = precedent_noeud_liste(AVisiter, nc);
+        float distance_nc = distance_noeud_liste(AVisiter, nc);
+        inserer_noeud_liste(Visites, nc, precedent_nc, distance_nc);
+        supprimer_noeud_liste(AVisiter, nc);
+        
+        size_t nvoisins = nombre_voisins(graphe, nc);
+        noeud_id_t* voisins = malloc(nvoisins*sizeof(noeud_id_t));
+        noeuds_voisins(graphe, nc, voisins);
+
+        for (int i = 0; i < nvoisins; i++)
+        {
+            noeud_id_t nv = voisins[i];
+            float nouvelle_distance = noeud_distance(graphe, nc, nv) + distance_nc;
+            float ancienne_distance = distance_noeud_liste(AVisiter, nv);
+            if (nouvelle_distance < ancienne_distance)
+            {
+                changer_noeud_liste(AVisiter, nv, nc, nouvelle_distance);
+            }
+        }
+    }
+}
+
+
+
---
+++ liste_noeud.c
@@ -0,0 +1,176 @@
+#define _GNU_SOURCE
+#include "liste_noeud.h"
+#include <stdlib.h>
+#include <math.h>
+
+liste_noeud_t* creer_liste(){
+    liste_noeud_t* liste = (liste_noeud_t*) malloc(sizeof(_cellule));
+    liste->debut = NULL;
+    return liste;
+}
+
+void detruire_liste(liste_noeud_t* liste_ptr){
+
+    _cellule* curseur = liste_ptr->debut;
+    _cellule* a_detruire;
+    while (curseur != NULL){
+        a_detruire = curseur;
+        curseur = curseur->suivante;
+        free(a_detruire);
+    }
+    liste_ptr->debut = NULL;
+}
+
+bool est_vide_liste(const liste_noeud_t* liste){
+    return liste->debut == NULL;
+}
+
+bool contient_noeud_liste(const liste_noeud_t* liste, noeud_id_t noeud){
+    _cellule* curseur = liste->debut;
+    while (curseur != NULL)
+    {
+        if (curseur->noeud == noeud)
+        {
+            return true;
+        }
+        curseur = curseur->suivante;
+    }
+    return false;
+}
+
+bool contient_arrete_liste(const liste_noeud_t* liste, noeud_id_t source, noeud_id_t destination){
+    _cellule* curseur = liste->debut;
+    while (curseur != NULL)
+    {
+        if (curseur->noeud == destination && curseur->precedent == source)
+        {
+            return true;
+        }
+        curseur = curseur->suivante;
+    }
+    return false;
+}
+
+float distance_noeud_liste(const liste_noeud_t* liste, noeud_id_t noeud){
+    _cellule* curseur = liste->debut;
+    while (curseur != NULL)
+    {
+        if (curseur->noeud == noeud)
+        {
+            return curseur->distance;
+        }
+        curseur = curseur->suivante;
+    }
+    return INFINITY;
+}
+
+noeud_id_t precedent_noeud_liste(const liste_noeud_t* liste, noeud_id_t noeud){
+    _cellule* curseur = liste->debut;
+    while (curseur != NULL)
+    {
+        if (curseur->noeud == noeud)
+        {
+            return curseur->precedent;
+        }
+        curseur = curseur->suivante;
+    }
+    return NO_ID;
+}
+
+noeud_id_t min_noeud_liste(const liste_noeud_t* liste){
+    _cellule* curseur = liste->debut;
+    float distance_min = INFINITY;
+    float noeud_min = NO_ID;
+    while (curseur != NULL)
+    {
+        if (curseur->distance < distance_min)
+        {
+            distance_min = curseur->distance;
+            noeud_min = curseur->noeud;
+        }
+        curseur = curseur->suivante;
+    }
+    return noeud_min;
+}
+
+void inserer_noeud_liste(liste_noeud_t* liste, noeud_id_t noeud, noeud_id_t precedent, float distance){
+    _cellule* nouveau_noeud = (_cellule*) malloc(sizeof(_cellule));
+    nouveau_noeud->noeud = noeud;
+    nouveau_noeud->distance = distance;
+    nouveau_noeud->precedent = precedent;
+    nouveau_noeud->suivante = NULL;
+
+    _cellule* curseur = liste->debut;
+    if(curseur == NULL){
+        liste->debut = nouveau_noeud;
+    } else {
+        while (curseur->suivante != NULL)
+        {
+            curseur = curseur->suivante;
+        }
+        curseur->suivante = nouveau_noeud;
+    } 
+    
+    
+}
+
+void changer_noeud_liste(liste_noeud_t* liste, noeud_id_t noeud, noeud_id_t precedent, float distance){
+    _cellule* curseur = liste->debut;
+
+    if(curseur == NULL){
+        _cellule* nouveau_noeud = (_cellule*) malloc(sizeof(_cellule));
+        nouveau_noeud->noeud = noeud;
+        nouveau_noeud->distance = distance;
+        nouveau_noeud->precedent = precedent;
+        nouveau_noeud->suivante = NULL;
+        liste->debut = nouveau_noeud;
+    } else {
+        _cellule* previous = curseur;
+        bool found = false;
+        while (curseur != NULL)
+        {
+            if (curseur->noeud == noeud){
+                curseur->precedent = precedent;
+                curseur->distance = distance;
+                found = true;
+            }
+            previous = curseur;
+            curseur = curseur->suivante;
+        }
+        if(!found){
+            _cellule* nouveau_noeud = (_cellule*) malloc(sizeof(_cellule));
+            nouveau_noeud->noeud = noeud;
+            nouveau_noeud->distance = distance;
+            nouveau_noeud->precedent = precedent;
+            nouveau_noeud->suivante = NULL;
+            previous->suivante = nouveau_noeud;
+        } 
+    }  
+    
+} 
+
+void supprimer_noeud_liste(liste_noeud_t* liste, noeud_id_t noeud){
+    _cellule* curseur = liste->debut;
+
+    _cellule* previous = NULL;
+    _cellule* adetruire;
+    bool found = false;
+    while (curseur != NULL && !found)
+    {
+        if (curseur->noeud == noeud){
+            adetruire = curseur;
+            found = true;
+            if (previous)
+            {
+                previous->suivante = curseur->suivante;
+            } else{
+                liste->debut = curseur->suivante;
+            }
+        }
+        previous = curseur;
+        curseur = curseur->suivante;
+    }
+    if(found){
+        free(adetruire);
+    } 
+} 
